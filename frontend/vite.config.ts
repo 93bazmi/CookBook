f1ae9b07846c42f2e8e407160ee96851684fd73f
santhod <santhod11111@gmail.com>
2025-02-15 19:56:46 +0700

    เพิ่ม Dockerfile และ docker-compose.yml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // ให้ Docker เปิดพอร์ตให้เข้าถึงจากภายนอก
+    port: 5173, // ใช้พอร์ตเดียวกับใน docker-compose.yml
+    strictPort: true, // ป้องกันการเปลี่ยนพอร์ตอัตโนมัติ
+    watch: {
+      usePolling: true, // แก้ปัญหาการตรวจจับไฟล์เปลี่ยนแปลงใน Docker
+    },
+  },
+});
