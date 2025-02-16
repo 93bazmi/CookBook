a7ff2d236eb4bdac756dab7234cda19c0e50b052
93bazmi <bambamboom234@gmail.com>
2025-02-17 00:50:26 +0700

    Update Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -75,85 +75,107 @@ const App = () => {
   };
 
   return (
-    <div className="app-container">
+    <div>
       <div className="header">
-        <img src="/bus1.jpg"></img>
-        <div className="title">Bus Kin Kap Arai Ko Aroi :3</div>
+        <img src="/logo.png" alt="Logo" className="logo"></img>
       </div>
-      <div className="tabs">
-        <h1
-          className={selectedTab === "search" ? "tab-active" : ""}
+
+      <div className="tabs flex gap-10">
+        <div
+          style={{ fontSize: "20px" }}
+          className={`tab px-6 py-2 rounded ${
+            selectedTab === "search" ? "tab-active" : "tab-s"
+          }`}
           onClick={() => setSelectedTab("search")}
         >
           Recipe Search
-        </h1>
-        <h1
-          className={selectedTab === "favourites" ? "tab-active" : ""}
+        </div>
+        <div
+          style={{ fontSize: "20px" }}
+          className={`tab px-6 py-2 rounded ${
+            selectedTab === "favourites" ? "tab-active" : "tab-f"
+          }`}
           onClick={() => setSelectedTab("favourites")}
         >
           Favourites
-        </h1>
+        </div>
       </div>
 
-      {selectedTab === "search" && (
-        <>
-          <form onSubmit={(event) => handleSearchSubmit(event)}>
-            <input
-              type="text"
-              required
-              placeholder="Enter a search term ..."
-              value={searchTerm}
-              onChange={(event) => setSearchTerm(event.target.value)}
-            ></input>
-            <button type="submit">
-              <AiOutlineSearch size={40} />
+      <div className="app-container">
+        {selectedTab === "search" && (
+          <>
+            <form onSubmit={(event) => handleSearchSubmit(event)}>
+              <input
+                type="text"
+                required
+                placeholder="Enter a search term ..."
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+              ></input>
+              <button type="submit">
+                <AiOutlineSearch size={40} />
+              </button>
+            </form>
+
+            <div className="recipe-grid">
+              {recipes.map((recipe) => {
+                const isFavourite = favouriteRecipes.some(
+                  (favRecipe) => recipe.id === favRecipe.id
+                );
+
+                return (
+                  <RecipeCard
+                    recipe={recipe}
+                    onClick={() => setSelectedRecipe(recipe)}
+                    onFavouriteButtonClick={
+                      isFavourite ? removeFavouriteRecipe : addFavouriteRecipe
+                    }
+                    isFavourite={isFavourite}
+                  />
+                );
+              })}
+            </div>
+
+            <button
+              style={{
+                marginBottom: "150px",
+                marginTop: "50px",
+                fontFamily: "'Neucha', serif",
+              }}
+              className="view-more-button"
+              onClick={handleViewMoreClick}
+            >
+              View More
             </button>
-          </form>
+          </>
+        )}
 
-          <div className="recipe-grid">
-            {recipes.map((recipe) => {
-              const isFavourite = favouriteRecipes.some(
-                (favRecipe) => recipe.id === favRecipe.id
-              );
-
-              return (
+        <div
+          style={{
+            marginBottom: "150px",
+          }}
+        >
+          {selectedTab === "favourites" && (
+            <div className="recipe-grid">
+              {favouriteRecipes.map((recipe) => (
                 <RecipeCard
                   recipe={recipe}
                   onClick={() => setSelectedRecipe(recipe)}
-                  onFavouriteButtonClick={
-                    isFavourite ? removeFavouriteRecipe : addFavouriteRecipe
-                  }
-                  isFavourite={isFavourite}
+                  onFavouriteButtonClick={removeFavouriteRecipe}
+                  isFavourite={true}
                 />
-              );
-            })}
-          </div>
+              ))}
+            </div>
+          )}
 
-          <button className="view-more-button" onClick={handleViewMoreClick}>
-            View More
-          </button>
-        </>
-      )}
-
-      {selectedTab === "favourites" && (
-        <div className="recipe-grid">
-          {favouriteRecipes.map((recipe) => (
-            <RecipeCard
-              recipe={recipe}
-              onClick={() => setSelectedRecipe(recipe)}
-              onFavouriteButtonClick={removeFavouriteRecipe}
-              isFavourite={true}
+          {selectedRecipe ? (
+            <RecipeModal
+              recipeId={selectedRecipe.id.toString()}
+              onClose={() => setSelectedRecipe(undefined)}
             />
-          ))}
+          ) : null}
         </div>
-      )}
-
-      {selectedRecipe ? (
-        <RecipeModal
-          recipeId={selectedRecipe.id.toString()}
-          onClose={() => setSelectedRecipe(undefined)}
-        />
-      ) : null}
+      </div>
     </div>
   );
 };
